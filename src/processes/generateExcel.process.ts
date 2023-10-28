787478b00536295cccd88df846c96f7052c88db6
hariscs4997 <hariscs4997@gmail.com>
2023-10-29 04:48:02 +0500

    changes done

--- src/processes/generateExcel.process.ts
+++ src/processes/generateExcel.process.ts
@@ -3,16 +3,21 @@ import ExcelJS from 'exceljs';
 import { config } from "../config/dbconfig";
 import sql from "mssql/msnodesqlv8";
 import fs from "fs"
+import JSZip from "jszip"
 
 const sqlService: any = sql
 
 const generateExcelProcess = async (job: Job) => {
     // console.log(job.data)
-    const { title, fileName } = job.data;
+    const { size, title, fileName } = job.data;
     const viewName = await getViewName(title);
-     let totalRecords:any = await getTotalRecords(viewName);
+    let totalRecords: any = await getTotalRecords(viewName);
     //if records are more than a million return.
-    if (totalRecords > 1000000) return false;
+    let iterations = totalRecords;
+    if (size > 0) {
+        iterations = Math.ceil(totalRecords / size);
+    }
+    const zip = new JSZip();
     //create a workbook
     const workbook = new ExcelJS.Workbook();
     if (!fs.existsSync(`public/excel-${job.id}`)) {
@@ -21,64 +26,69 @@ const generateExcelProcess = async (job: Job) => {
     //create a worksheet
     const worksheet = workbook.addWorksheet("Sheet");
     const result = await getAllData(viewName)
-    // let result:any = []
-    //     let itemCount = Math.floor(totalRecords/100000) + 1
-    //     for(var i = 1;i<itemCount;i++){
-    //     const rowsToSkip = (i - 1) * 100000;
-    //     const data = await getAllData(rowsToSkip,100000);
-    //     console.log(data)
-    //     result = result.concat(data)
-    //     }
-    // console.log(result)
-    //prepare columns
-    const columns = Object.keys(result[0]).map((col) => {
-        return {
-            name: col,
-            filterButton: true,
-        };
-    });
+    for (let i = 0; i < iterations; i++) {
+        const columns = Object.keys(result[0]).map((col) => {
+            return {
+                name: col,
+                filterButton: true,
+            };
+        });
 
-    worksheet.properties.defaultRowHeight = 20;
+        worksheet.properties.defaultRowHeight = 20;
 
-    //add data
-    worksheet.addTable({
-        name: "MyTable",
-        ref: "A1",
-        headerRow: true,
-        totalsRow: false,
-        style: {
-            theme: "TableStyleMedium2",
-            showRowStripes: true,
-            showFirstColumn: true,
-        },
-        columns,
-        rows: result.map((item: any) => {
-            let data: any = [];
-            Object.keys(item).map((key) => {
-                data.push(item[key] ? item[key] : "");
-            });
-            return data;
-        }),
-    });
+        //add data
+        worksheet.addTable({
+            name: "MyTable",
+            ref: "A1",
+            headerRow: true,
+            totalsRow: false,
+            style: {
+                theme: "TableStyleMedium2",
+                showRowStripes: true,
+                showFirstColumn: true,
+            },
+            columns,
+            rows: result.map((item: any) => {
+                let data: any = [];
+                Object.keys(item).map((key) => {
+                    data.push(item[key] ? item[key] : "");
+                });
+                return data;
+            }),
+        });
 
-    //stylings
-    worksheet.columns.forEach((column, index) => {
-        column.width = columns[index].name.length * 1.5;
-    });
-    worksheet.eachRow((row) => {
-        row.font = {
-            size: 13,
+        //stylings
+        worksheet.columns.forEach((column, index) => {
+            column.width = columns[index].name.length * 1.5;
+        });
+        worksheet.eachRow((row) => {
+            row.font = {
+                size: 13,
+                name: "Arial",
+            };
+        });
+        worksheet.getRow(1).font = {
             name: "Arial",
+            size: 15,
+            bold: true,
         };
-    });
-    worksheet.getRow(1).font = {
-        name: "Arial",
-        size: 15,
-        bold: true,
-    };
-    //wrtie file
-    await workbook.xlsx.writeFile(`public/excel-${job.id}/${fileName}.xlsx`);
-    return `${fileName}.xlsx`
+        //wrtie file
+        await workbook.xlsx.writeFile(`public/excel-${job.id}/${fileName}-${i}.xlsx`).then(() => {
+            zip.file(
+                `${fileName}-${i}.xlsx`,
+                fs.readFileSync(`public/excel-${job.id}/${fileName}-${i}.xlsx`)
+            );
+            if (i === iterations - 1) {
+                zip
+                    .generateNodeStream({ type: "nodebuffer", streamFiles: true })
+                    .pipe(fs.createWriteStream(`public/excel-${job.id}/${fileName}.zip`))
+                    .on("finish", function () {
+                        return `public/excel-${job.id}/${fileName}.zip`
+                    });
+            }
+        })
+
+    }
 }
 const getViewName = async (title: string) => {
     try {
@@ -92,7 +102,7 @@ const getViewName = async (title: string) => {
         console.log(error);
     }
 }
-const getTotalRecords = async (viewName:string) => {
+const getTotalRecords = async (viewName: string) => {
     try {
         const pool = await sqlService.connect(config);
         const totalRecords = await pool
@@ -103,7 +113,7 @@ const getTotalRecords = async (viewName:string) => {
         console.error(error);
     }
 }
-const getAllData = async (viewName:string) => {
+const getAllData = async (viewName: string) => {
     try {
         const pool = await sqlService.connect(config);
         const products = await pool.request().query(

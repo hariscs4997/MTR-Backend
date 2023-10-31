a0341f5d1b0e03722ebfe807aceb1b0ebe7191b3
hariscs4997 <hariscs4997@gmail.com>
2023-10-31 19:07:37 +0500

    changes done

--- src/api/routes/api/tagsQuickFind/index.ts
+++ src/api/routes/api/tagsQuickFind/index.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/get-tag-class-data', TagsQuickFindController.getTagsQuickTagClassData);
 router.post('/get-tag-sub-class-data', TagsQuickFindController.getTagsQuickTagSubClassData);
 router.post('/get-tag-type-data', TagsQuickFindController.getTagsQuickTagTypeData);
-router.post('/get-main-unit-data', TagsQuickFindController.getTagsQuickFindMainUnitData);
+router.get('/get-main-unit-data', TagsQuickFindController.getTagsQuickFindMainUnitData);
 router.post('/get-process-unit-data', TagsQuickFindController.getTagsQuickFindProcessUnitData);
 router.post('/get-data', TagsQuickFindController.getTagsQuickFindTableData);
 router.post('/get-tag-class-data', TagsQuickFindController.getTagsQuickTagClassData);

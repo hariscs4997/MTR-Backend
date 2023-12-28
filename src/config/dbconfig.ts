a74a65eecc5717ad8b8f447385df37b2db3d8304
hariscs4997 <hariscs4997@gmail.com>
2023-12-28 20:36:13 +0500

    changes done

--- src/config/dbconfig.ts
+++ src/config/dbconfig.ts
@@ -1,13 +1,13 @@
 export const config = {
-    user: 'MTRDevSA',
-    password: 'bGcBX645CWII',
-    database:  'MTR_DEV',
-    server: '20.116.30.16',
+    user: 'mtrdev',
+    password: 'B1%=]fV2cEj9',
+    database:  'mtrdev',
+    server: 'cooleycoreme.database.windows.net',
     driver: 'msnodesqlv8',
     port: 1433,
     options: {
 //      trustedconnection:  true
-      trustedconnection:  false
+      trustedconnection:  true
     }
   };
 
